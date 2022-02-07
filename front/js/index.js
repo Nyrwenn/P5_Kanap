db674481b126723d5cac7dc86f87b43cf4861f33
Fanny <fannyy.paris@gmail.com>
2022-02-07 17:54:16 +0100

    page d'accueil opérationnelle

--- front/js/index.js
+++ front/js/index.js
@@ -1,13 +1,45 @@
-//Mise en place d'un fetch afin de récupérer tous les produits sur l'API//
+//Mise en place d'un fetch afin de récupérer tous les produits sur l'API
+//le fetch est mis dans une fonction qui sera par la suite passée dans une autre 
+//fonction asynchrone afin de pouvoir passer un paramètre "await" à la récupération 
+//des valeurs de façon à ce qu'elles soient récupérées avant la fin du script.
+const getProducts = () =>
+    fetch('http://localhost:3000/api/products')
+        .then(res => res.json())
+        .then(data => data)
+        .catch(function (err) {
+            console.log('erreur de chargement...', err)
+        });
 
-fetch('http://localhost:3000/api/products')
-    .then(function (res) {
-        return res.json();
-    })
-    .then(function (value) {
-        console.log(value);
-    })
-    .catch(function (err) {
-        console.log('error', err);
-    });
+//catch qui nous permet de capter une erreur si il s'en produit une lors de l'exécution du fetch
 
+
+//boucle mise dans une fonction, qui sera aussi passée dans fonction main asynchrone
+//les données seront donc récupérées avant la fin du script
+
+const displayProducts = (allProducts) => {
+    for (let i in allProducts) {
+
+        //je définis une const pour que la récupération de mes éléments dans l'API soit plus simple
+        const product = allProducts[i];
+        let element = document.getElementById("items");
+        let childElement = document.createElement("a");
+
+        childElement.innerHTML = `<a href="./product.html?id=${product._id}"><article>
+        <img src=${product.imageUrl} alt=${product.altTxt}>
+        <h3 class="productName">${product.name}</h3>
+        <p class="productDescription">${product.description}</p>
+        </article></a>`;
+
+        element.appendChild(childElement);
+        //appendChild me permet de créer un nouveau noeud html pour chaque produit
+    }
+
+}
+
+
+const main = async () => {
+    const allProducts = await getProducts();
+    const displayer = await displayProducts(allProducts);
+}
+
+main();
