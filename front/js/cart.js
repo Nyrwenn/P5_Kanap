a983317bcd0234e4b1c36bc0e77701fd36851da3
Fanny <fannyy.paris@gmail.com>
2022-02-11 15:43:45 +0100

    modification option suppression

--- front/js/cart.js
+++ front/js/cart.js
@@ -8,10 +8,10 @@ let storage = JSON.parse(localStorage.getItem("cart"));
 const element = document.getElementById("cart__items");
 
 const displayProducts = (products, storageToShow) => {
-    let childElement = document.createElement("article")
-    childElement.class = "cart__item";
-    childElement["data-id"] = storageToShow.id;
-    childElement["data-color"] = storageToShow.color;
+    let childElement = document.createElement("article");
+    childElement.classList.add("cart__item");
+    childElement.dataset.id = storageToShow.id;
+    childElement.dataset.color = storageToShow.color;
 
     childElement.innerHTML = `<div class="cart__item__img">
       <img src=${products.imageUrl} alt=${products.altTxt}>
@@ -28,60 +28,79 @@ const displayProducts = (products, storageToShow) => {
           <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${storageToShow.quantity}>
         </div>
         <div class="cart__item__content__settings__delete">
-          <p class="deleteItem" data-color="${storageToShow.color}" data-id="${storageToShow.id}" id="${storageToShow.id}${storageToShow.color}">Supprimer</p>
+          <p class="deleteItem">Supprimer</p>
         </div>
       </div>
     </div>`
 
     element.appendChild(childElement);
-
-    /* Je viens récupérer l'élément avec mes deux clés id et color, j'ajoute un event listener au clic 
-    pour savoir quand quelqu'un a cliqué sur supprimer.
-    A la suite de ça je vais comparer les data récupérées par le clic et celles de mon storage pour la suppression*/
-
-    const keepElement = document.getElementById(`${storageToShow.id}${storageToShow.color}`);
-    keepElement.addEventListener("click", function () {
-        const dataColor = keepElement.dataset.color;
-        const dataId = (keepElement.dataset.id);
-
-        for (let i in storage) {
-            if (storage[i].id === dataId && storage[i].color === dataColor) {
-                storage.splice(i, 1);
-
-                /*ne pas oublier de récupérer la valeur et de mettre à jour mon local storage*/
-
-                try {
-                    localStorage.setItem("cart", JSON.stringify(storage));
-                    location.reload();
-                } catch (err) {
-                    console.log("error", err);
-                }
-            }
-
-        }
-    })
 }
 
 /*Je vais récupérer  mes données API pour avoir les clés pour afficher le panier utilisateur*/
 
-for (let storageToShow of storage) {
+async function displayAllProducts() {
+    for (let storageToShow of storage) {
 
-    const getProducts = () =>
-        fetch(`http://localhost:3000/api/products/${storageToShow.id}`)
-            .then(res => res.json())
-            .then(data => data)
-            .catch(function (err) {
-                console.log("error", err);
-            });
+        const getProducts = () =>
+            fetch(`http://localhost:3000/api/products/${storageToShow.id}`)
+                .then(res => res.json())
+                .then(data => data)
+                .catch(function (err) {
+                    console.log("error", err);
+                });
 
-    const main = async () => {
-        const products = await getProducts();
-        const displayer = await displayProducts(products, storageToShow);
+        /*J'attends que la fonction ait fini de m'afficher tous les éléments*/
+
+        const displayOneElt = async () => {
+            const products = await getProducts();
+            const displayer = await displayProducts(products, storageToShow);
+        }
+
+        await displayOneElt();
     }
-
-    main();
-
+    return
 }
+
+/* Fonction asynchrone qui me permet d'attendre que tous les produits soient affichés
+avant de passer à la suite du code*/
+
+async function main() {
+    const displayAll = await displayAllProducts();
+
+    /*Je cible les éléments à écouter pour déterminer les produits à supprimer*/
+    let elementsDelete = document.querySelectorAll(".deleteItem");
+
+    for (elt of elementsDelete) {
+        elt.addEventListener("click", function (event) {
+            /*event target cible précisément l'élément, closest me permet de récupérer les id et
+            les colors de l'élément parent*/
+            const keepElement = event.target.closest("article");
+            const dataColor = keepElement.dataset.color;
+            const dataId = keepElement.dataset.id;
+            //Coda avant
+            for (let i in storage) {
+                if (storage[i].id === dataId && storage[i].color === dataColor) {
+                    storage.splice(i, 1);
+
+                    /*ne pas oublier de récupérer la valeur et de mettre à jour mon local storage*/
+
+                    try {
+                        localStorage.setItem("cart", JSON.stringify(storage));
+                        location.reload();
+                    } catch (err) {
+                        console.log("error", err);
+                    }
+                }
+
+            }
+
+        })
+    }
+}
+
+main();
+
+
 
 
 
